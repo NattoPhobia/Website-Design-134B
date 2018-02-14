9b3e73f55285ec2464bfd3c586fcc41fb066d7b8
nac018 <32497892+nac018@users.noreply.github.com>
2018-02-14 01:54:53 -0800

    Add files via upload

---
+++ js/Agent.js
@@ -0,0 +1,11 @@
+function directToHome(){
+	window.location = "../html/UserProfile.html";
+}
+
+function directToChat(){
+	window.location = "../html/Chat.html";
+}
+
+function directToChooseAgent(){
+	window.location.replace("");
+} 
