5ddc51a886b58a486057bf8952e7b7c914b8441b
NattoPhobia <hwn001@ucsd.edu>
2018-02-17 00:34:47 -0800

    Implemented agent object and loadAgent().

--- js/Agent.js
+++ js/Agent.js
@@ -9,3 +9,26 @@ function directToChat(){
 function directToChooseAgent(){
 	window.location = "../html/ChooseAgent.html";
 } 
+
+function loadAgents(agents){
+	for (var i = 0; i < agents.length;i++) {
+		var agt = agents[i];
+
+		document.getElementByClassName("agent").innerHTML += '<div class="agent_left_panel"> <img class="agent_photo" src="' + agt.image_url + 
+			'" alt=""> </div><div class="agent_right_panel"> <h3>' + agt.name + '</h3> <p>' + agt.stars + '</p> <h5>' + agt.address + '</h5></div> <div class="agent_rightmost_panel"><button type="reset" style="width: 35px; height: 35px; margin-top: 55px; font-weight: bold" onclick="directToChooseAgent()">></button></div>';
+	}
+}
+
+function uniqueAgent(name, stars, address image_url){
+	this.name = name;
+	this.stars = stars;
+	this.address = address;
+	this.image_url = image_url;
+}
+
+var agent1 = new uniqueAgent("John", "★★★☆☆", "San Maeto, CA", "../resources/default_Profile.jpg");
+var agent2 = new uniqueAgent("Reggin", "★★★★☆", "Oakland, CA", "../resources/default_Profile.jpg");
+var agent3 = new uniqueAgent("Claire", "★★☆☆☆", "San Francisco, CA", "../resources/default_Profile.jpg");
+var agent4 = new uniqueAgent("Isaac", "★★★★★", "San Diego, CA", "../resources/default_Profile.jpg");
+
+loadAgents([agent1,agent2,agent3,agent4]); 
